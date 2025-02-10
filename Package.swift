d8ce5e133450f55e9bf09f99151d9586b493e18a
Felix Andrew <felixandrew@1791entertainment.com>
2025-02-10 15:37:07 -0700

    package dependency

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
       // Add your dependency here
       .package(url: "https://github.com/flexi1791/Helpers1791.git", from: "1.0.0"),
-    ],
+    ], 
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
