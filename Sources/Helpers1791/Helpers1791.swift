88043e898b6f749ff3409628e893d080296e3043
Felix Andrew <felixandrew@1791entertainment.com>
2025-02-10 14:57:54 -0700

    FallbackImage

--- Sources/Helpers1791/Helpers1791.swift
+++ Sources/Helpers1791/Helpers1791.swift
@@ -1,2 +1,23 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+
+public struct FallbackImage {
+  public let image: Image
+  
+  public init(_ filename: String) {
+    if let uiImage = UIImage(named: filename) {
+      self.image = Image(uiImage: uiImage)
+    } else {
+      let bundle = Bundle(identifier: "com.1791entertainment.Corporate-Mystic.SharedResoures")
+      if let uiImage = UIImage(named: filename, in: bundle, compatibleWith: nil) {
+        self.image = Image(uiImage: uiImage)
+      } else {
+        self.image = Image(systemName: "xmark")
+        print("Cannot load \(filename) from bundle \(String(describing: bundle))")
+      }
+    }
+  }
+}
+
