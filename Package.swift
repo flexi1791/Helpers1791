d7b414b2880ac5afcd1237c70400b5a8162937a7
Felix Andrew <felixandrew@1791entertainment.com>
2025-02-10 15:33:46 -0700

    Git repo dependency

--- Package.swift
+++ Package.swift
@@ -14,6 +14,10 @@ let package = Package(
             name: "Helpers1791",
             targets: ["Helpers1791"]),
     ],
+    dependencies: [
+      // Add your dependency here
+      .package(url: "https://github.com/flexi1791/Helpers1791.git", from: "1.0.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
